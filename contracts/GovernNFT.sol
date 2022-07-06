0c1258c0af736ee8d5c1bd492c80e0415a22702f
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-06 16:41:28 +0530

    added GovernNFT contract

---
+++ contracts/GovernNFT.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.0/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
+
+contract MyToken is ERC721, EIP712, ERC721Votes {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    address payable vault;
+
+    constructor(address payable _vault)
+        ERC721("MyToken", "MTK")
+        EIP712("MyToken", "1")
+    {
+        vault = _vault;
+    }
+
+    function safeMint(address to) public payable {
+        require(msg.value >= 1 ether, "1 ether required to mint");
+        vault.transfer(address(this).balance);
+
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Votes) {
+        super._afterTokenTransfer(from, to, tokenId);
+    }
+}
