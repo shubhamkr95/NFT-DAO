860479bd47f003d26e3b5cf4b94db66deb2fa526
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-07 19:13:28 +0530

    deploy contract factory in test file

--- test/dao.test.js
+++ test/dao.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { mineBlocks } = require("../mineBlock");
+const { mineBlocks } = require("../utils/mineBlock");
 
 describe("Govern", async () => {
  let Treasury;
@@ -12,4 +12,30 @@ describe("Govern", async () => {
  let GovernorContract;
  let governorContract;
  let proposalID;
+
+ beforeEach(async () => {
+  [vote1, vote2, vote3, vote4] = await ethers.getSigners();
+
+  // Treasury contract
+  Treasury = await ethers.getContractFactory("Treasury");
+  treasury = await Treasury.deploy();
+  await treasury.deployed();
+
+  // GovernToken
+  GovernToken = await ethers.getContractFactory("GovernToken");
+  governToken = await GovernToken.deploy(treasury.address);
+  await governToken.deployed();
+
+  //Timelock
+  TimeLock = await ethers.getContractFactory("TimeLock");
+  timelock = await TimeLock.deploy(0, [], ["0x0000000000000000000000000000000000000000"]);
+  await timelock.deployed();
+
+  // Governor
+  GovernorContract = await ethers.getContractFactory("Governance");
+  governorContract = await GovernorContract.deploy(governToken.address, timelock.address);
+  await governorContract.deployed();
+
+  await treasury.transferOwnership(timelock.address);
+ });
 });
