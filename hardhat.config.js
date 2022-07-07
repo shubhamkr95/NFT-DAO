0ff39ac4cda5803e4ef172fef08641d5ed719875
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-07 19:11:49 +0530

    hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("solidity-coverage");
+require("hardhat-contract-sizer");
 
 module.exports = {
- solidity: "0.8.4",
- settings: {
-  optimizer: {
-   enabled: true,
-   runs: 200,
+ solidity: {
+  version: "0.8.4",
+  settings: {
+   optimizer: {
+    enabled: true,
+    runs: 200,
+   },
   },
+ },
+ contractSizer: {
+  alphaSort: true,
+  runOnCompile: true,
+  disambiguatePaths: false,
  },
  plugins: ["solidity-coverage"],
 };
