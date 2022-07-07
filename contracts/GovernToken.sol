d73bf50569a4aa7a11b303c39d3a13167acca388
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-07 15:14:59 +0530

    prettier style applied to contracts

--- contracts/GovernToken.sol
+++ contracts/GovernToken.sol
@@ -7,33 +7,33 @@ import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/draft-ERC721Votes.
 import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 
 contract GovernToken is ERC721, EIP712, ERC721Votes {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+ using Counters for Counters.Counter;
+ Counters.Counter private _tokenIdCounter;
 
-    address payable vault;
+ address payable vault;
 
-    constructor(address payable _vault)
-        ERC721("GovernToken", "GTK")
-        EIP712("GovernToken", "1")
-    {
-        vault = _vault;
-    }
+ constructor(address payable _vault)
+  ERC721("GovernToken", "GTK")
+  EIP712("GovernToken", "1")
+ {
+  vault = _vault;
+ }
 
-    function safeMint(address to) public payable {
-        require(msg.value >= 1 ether, "1 ether required to mint");
-        vault.transfer(address(this).balance);
+ function safeMint(address to) public payable {
+  require(msg.value >= 1 ether, "1 ether required to mint");
+  vault.transfer(address(this).balance);
 
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-    }
+  uint256 tokenId = _tokenIdCounter.current();
+  _tokenIdCounter.increment();
+  _safeMint(to, tokenId);
+ }
 
-    // The following functions are overrides required by Solidity.
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Votes) {
-        super._afterTokenTransfer(from, to, tokenId);
-    }
+ // The following functions are overrides required by Solidity.
+ function _afterTokenTransfer(
+  address from,
+  address to,
+  uint256 tokenId
+ ) internal override(ERC721, ERC721Votes) {
+  super._afterTokenTransfer(from, to, tokenId);
+ }
 }
