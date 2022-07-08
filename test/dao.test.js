5c5300fbeba315ace97c9b4179e42944222e7f60
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-08 16:19:26 +0530

    added deployment in test file and delegate the token

--- test/dao.test.js
+++ test/dao.test.js
@@ -12,18 +12,17 @@ describe("Govern", async () => {
  let GovernorContract;
  let governorContract;
  let proposalID;
+ let tokenID;
+ let delegateTxn;
+ let proposalRole;
+ let grantProposalRole;
 
  beforeEach(async () => {
-  [vote1, vote2, vote3, vote4] = await ethers.getSigners();
-
-  // Treasury contract
-  Treasury = await ethers.getContractFactory("Treasury");
-  treasury = await Treasury.deploy();
-  await treasury.deployed();
+  [sender, address1, address2, address3, address4] = await ethers.getSigners();
 
   // GovernToken
   GovernToken = await ethers.getContractFactory("GovernToken");
-  governToken = await GovernToken.deploy(treasury.address);
+  governToken = await GovernToken.deploy();
   await governToken.deployed();
 
   //Timelock
@@ -33,9 +32,48 @@ describe("Govern", async () => {
 
   // Governor
   GovernorContract = await ethers.getContractFactory("Governance");
-  governorContract = await GovernorContract.deploy(governToken.address, timelock.address);
+  governorContract = await GovernorContract.deploy(
+   governToken.address,
+   timelock.address,
+   0, //voting delay -0 block delay
+   14, //voting period - 3 minute
+   1, //proposal threshold - 1 NFT
+   4 //quorum percentage
+  );
   await governorContract.deployed();
 
+  // Treasury contract
+  Treasury = await ethers.getContractFactory("Treasury");
+  treasury = await Treasury.deploy();
+  await treasury.deployed();
+
+  // mint nft to user
+  await governToken.safeMint(address1.address, "ipfs://bafybeibfhnw4uspjj2akxzkbeia4fxv37gkyvvs2skja5tomlcv3soo2hm/");
+
+  // send 5 ether to treasury
+  const txn = await sender.sendTransaction({
+   to: treasury.address,
+   value: ethers.utils.parseEther("5.0"),
+  });
+  await txn.wait();
+
   await treasury.transferOwnership(timelock.address);
+
+  // delegate the user
+  delegateTxn = await governToken.connect(address1).delegate(address1.address);
+  tokenID = delegateTxn.value.toString();
+ });
+
+ it("Should grant the proposal role", async () => {
+  proposalRole = await timelock.PROPOSER_ROLE();
+  grantProposalRole = await timelock.grantRole(proposalRole, governorContract.address);
+
+  expect(await timelock.hasRole(proposalRole, governorContract.address)).to.equal(true);
+ });
+
+ it("should transfer treasury ownership to timelock", async () => {
+  expect(await treasury.owner()).to.equal(timelock.address);
+
+  expect(await treasury.balance()).to.equal(ethers.utils.parseEther("5"));
  });
 });
