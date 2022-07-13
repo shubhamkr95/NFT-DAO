b2345fd9ce130723e765f9cc5205b73823d08c34
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-13 15:47:34 +0530

    modified deployment script

--- scripts/deployContracts.js
+++ scripts/deployContracts.js
@@ -3,14 +3,14 @@ const { ethers } = require("hardhat");
 async function main() {
  const [deployer] = await ethers.getSigners();
 
- console.log("Deploying Govern token contract with the account:", deployer.address);
+ console.log(`Deploying Govern token contract with the account: ${deployer.address} \n`);
 
  // Deploying token
  const GovernToken = await ethers.getContractFactory("GovernToken");
  const governToken = await GovernToken.deploy();
  await governToken.deployed();
 
- console.log(`GovernToken address ${governToken.address}`);
+ console.log(`GovernToken address ${governToken.address} \n`);
 
  //Deploying TimeLock
  const TimeLock = await ethers.getContractFactory("TimeLock");
@@ -21,21 +21,40 @@ async function main() {
  );
  await timeLock.deployed();
 
- console.log(`Timelock address ${timeLock.address}`);
+ console.log(`Timelock address ${timeLock.address} \n`);
 
  // Deploying Governor
  const GovernorContract = await ethers.getContractFactory("Governance");
  const governorContract = await GovernorContract.deploy(
   governToken.address,
   timeLock.address,
-  0, //voting delay -0 block delay
+  1, //voting delay -0 block delay
   14, //voting period - 3 minute
   1, //proposal threshold - 1 NFT
-  4 //quorum percentage
+  4 //quorum percentage - 4 percent
  );
  await governorContract.deployed();
 
- console.log(`Governor address ${governorContract.address}`);
+ console.log(`Governor address ${governorContract.address} \n`);
+
+ // Deploying Treasury
+ Treasury = await ethers.getContractFactory("Treasury");
+ treasury = await Treasury.deploy();
+ await treasury.deployed();
+
+ console.log(`Treasury address ${treasury.address} \n`);
+
+ // transfer proposal role to the governor address
+ const proposalRole = await timeLock.PROPOSER_ROLE();
+ await timeLock.grantRole(proposalRole, governorContract.address);
+
+ // revoke owner admin role from timelock
+ const timeLockAdminRole = await timeLock.TIMELOCK_ADMIN_ROLE();
+ await timeLock.revokeRole(timeLockAdminRole, deployer.address);
+
+ //transfer ownership of treasury to timelock address
+ await treasury.transferOwnership(timeLock.address);
+ console.log(`New Treasury owner is ${await treasury.owner()} \n`);
 }
 
 main()
