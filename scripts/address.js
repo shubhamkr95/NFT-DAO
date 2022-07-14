c0de38d773fce7052b61641f35a64ede7b3f9a37
shubhamkr95 <sahooshubham124@gmail.com>
2022-07-14 15:05:20 +0530

    removed address.js

--- scripts/address.js
+++
@@ -1,7 +0,0 @@
-// polygon mumbai
-{
- ("GovernToken address  = 0x2fFda8135aE1fc22b84EC6F8d185D8F3dFC9a352");
- ("Timelock address  = 0x15B6002738550e08a1896CF7ac99694031BF0dC8");
- ("Governor address  = 0xb38D990Ac50E51435f155FE263DBdC0B746F206F");
- ("Treasury address = 0x637F221a5E31b13446B027462E543245C3531090");
-}
