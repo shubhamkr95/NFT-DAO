ad6d3c9bcf34c099a559865cd3c207f45cf6d691
shubhamkr95 <sahooshubham124@gmail.com>
2022-08-02 12:14:56 +0530

    added queue and execute script

---
+++ scripts/queue.js
@@ -0,0 +1,35 @@
+const { ethers } = require("hardhat");
+const { GOVERNOR_ADDRESS, TREASURY_ADDRESS, PROPOSAL_ID } = require("./address.js");
+
+async function main() {
+ const [owner] = await ethers.getSigners();
+
+ // Get The governor address
+ const Governance = await ethers.getContractFactory("Governance");
+ const governance = await Governance.attach(GOVERNOR_ADDRESS);
+
+ // Get the treasury address
+ const Treasury = await ethers.getContractFactory("Treasury");
+ const treasury = await Treasury.attach(TREASURY_ADDRESS);
+
+ // check the state of the proposal before queue
+ console.log("Proposal state before queueing: ", await governance.state(PROPOSAL_ID));
+
+ // Now, queue the proposal.
+ const queueTxn = await governance.queue(
+  [TREASURY_ADDRESS],
+  [0],
+  [await treasury.interface.encodeFunctionData("withdrawFunds", [owner.address, ethers.utils.parseUnits("1", 18)])],
+  ethers.utils.keccak256(ethers.utils.toUtf8Bytes("Sending 1 ether to owner address!"))
+ );
+
+ await queueTxn.wait();
+ console.log("Queued the proposal!");
+}
+
+main()
+ .then(() => process.exit(0))
+ .catch((error) => {
+  console.error(error);
+  process.exit(1);
+ });
